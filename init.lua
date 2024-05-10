13996aec17c027fb593ce90536be17fcc3d7ff90
rakshyak-98 <rakshak1998@gmail.com>
2024-05-10 23:48:04 +0530

    tab with to 2

--- init.lua
+++ init.lua
@@ -33,6 +33,10 @@ vim.opt.showmode = false
 --  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
 
+-- Indenting
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
 -- Enable break indent
 vim.opt.breakindent = true
 
@@ -762,7 +766,7 @@ require('lazy').setup({
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        -- additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
